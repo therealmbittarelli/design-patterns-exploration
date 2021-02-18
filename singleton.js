f91a9db7b6fb75754ac2da5de76b89257d87b95d
therealmbittarelli <55117561+therealmbittarelli@users.noreply.github.com>
2021-02-18 16:57:05 -0500

    singleton updates

--- singleton.js
+++ singleton.js
@@ -1,13 +1,13 @@
 let Singleton = (function () {
     let instance;
 
-    function createInstance() {
+    createInstance = () => {
         let inst = "It's me! I'm the instance!";
         return inst;
     }
 
     return {
-        getInstance: function () {
+        getInstance() {
             if (!instance) {
                 instance = createInstance();
             }
@@ -16,15 +16,40 @@ let Singleton = (function () {
     };
 }) ();
 
-function run() {
-
-    let instanceOne = Singleton.getInstance();
-    let instanceTwo = Singleton.getInstance();
-
-    console.log('inst 1 is:', instanceOne)
-
-    let verdict = instanceOne === instanceTwo ? "It worked!" : "Try again.";
-    console.log(verdict);
+class SingletonClass {
+    constructor() {
+        if (!SingletonClass._instance) {
+            SingletonClass._instance = this;
+        }
+        return SingletonClass._instance;
+    }
+    
+    saySomethingFunny = () => {
+        console.log("I will attack you with the North.")
+    }
+    
 }
 
+
+run = () => {
+    
+    let instanceOneF = Singleton.getInstance();
+    let instanceTwoF = Singleton.getInstance();
+    
+    let instanceOneC = new SingletonClass();
+    let instanceTwoC = new SingletonClass();
+    instanceOneC.saySomethingFunny()
+
+    console.log('inst 1 is:', instanceOneF)
+
+    let verdictF = instanceOneF === instanceTwoF ? "Function: It worked!" : "Function: Try again.";
+    let verdictC = instanceOneC === instanceTwoC ? "Class: It worked!" : "Class: Try again.";
+    
+    console.log(verdictF);
+    console.log(verdictC);
+}
+
+
+
 run();
+
