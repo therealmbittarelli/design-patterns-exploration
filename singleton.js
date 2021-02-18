661c98e8802da00d5dc9a15c96ff2c860d855e41
therealmbittarelli <55117561+therealmbittarelli@users.noreply.github.com>
2021-02-18 16:35:51 -0500

    updating singleton.js, adding singleton.ts with notes and examples

--- singleton.js
+++ singleton.js
@@ -1,2 +1,30 @@
-// Restricts instantiation of a class to one object, providing global access to the one instance
+let Singleton = (function () {
+    let instance;
 
+    function createInstance() {
+        let inst = "It's me! I'm the instance!";
+        return inst;
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+}) ();
+
+function run() {
+
+    let instanceOne = Singleton.getInstance();
+    let instanceTwo = Singleton.getInstance();
+
+    console.log('inst 1 is:', instanceOne)
+
+    let verdict = instanceOne === instanceTwo ? "It worked!" : "Try again.";
+    console.log(verdict);
+}
+
+run();
---
+++ singleton.ts
@@ -0,0 +1,72 @@
+// Restricts instantiation of a class to one object, providing global access to the one instance
+
+// Cannot implement with a regular constructor as constructors must return a new object (by design)\
+// Default constructor is made private (can't use the 'new' operator to create a new instance)
+// Create static creation method to act as a constructor, which calls the private constructor to create an object and saves it in a static field (all future calls to the method return the cached object)
+
+// We use Singleton enforcement in Thunder and MyGiide (AppRoot.js, connectedCallback())
+
+// Singleton
+
+// -instance: Singleton
+// -Singleton()
+// getInstance(): Singleton
+
+// if (instance == null) {
+    // instance = new Singleton()
+// }
+// return instance
+
+
+
+// Usually, the goal is to manage global application state. Some examples I’ve seen or written myself include using a singleton as the source of config settings for a web app, on the client side for anything initiated with an API key (you usually don’t want to risk sending multiple analytics tracking calls, for example), and to store data in memory in a client-side web application (e.g. stores in Flux).
+
+class SingletonClass {
+    private static instance: SingletonClass;
+
+    /**
+     * The SingletonClass's constructor should always be private to prevent direct
+     * construction calls with the `new` operator.
+     */
+    private constructor() { }
+
+    /**
+     * The static method that controls the access to the singleton instance.
+     *
+     * This implementation let you subclass the Singleton class while keeping
+     * just one instance of each subclass around.
+     */
+    public static getInstance(): SingletonClass {
+        if (!SingletonClass.instance) {
+            SingletonClass.instance = new SingletonClass();
+        }
+
+        return SingletonClass.instance;
+    }
+
+    /**
+     * Finally, any singleton should define some business logic, which can be
+     * executed on its instance.
+     */
+    public someMethod() {
+        console.log("Inside of someMethod()")
+    }
+}
+
+/**
+ * The client code.
+ */
+function clientCode() {
+    const s1 = SingletonClass.getInstance();
+    const s2 = SingletonClass.getInstance();
+
+    s1.someMethod();
+
+    if (s1 === s2) {
+        console.log('SingletonClass works, both variables contain the same instance.');
+    } else {
+        console.log('SingletonClass failed, variables contain different instances.');
+    }
+}
+
+clientCode();
