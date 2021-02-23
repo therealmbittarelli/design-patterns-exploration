4bca58adcfd0290197b2323be56f245b6c5de048
therealmbittarelli <55117561+therealmbittarelli@users.noreply.github.com>
2021-02-23 17:52:14 -0500

    adding prototype.ts

---
+++ prototype.ts
@@ -0,0 +1,7 @@
+// Enables cloning of existing objects without having to become dependent on their classes
+
+// Example:
+// mitotic cell division
+// After mitotic division, a pair of identical cells exist.
+// The original cell is the prototype, and creates the copy.
+
