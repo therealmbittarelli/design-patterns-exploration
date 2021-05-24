f485c449fd3316df660be1ed77c6d0c6f112c227
therealmbittarelli <55117561+therealmbittarelli@users.noreply.github.com>
2021-05-24 13:10:24 -0400

    update decorator.ts

--- decorator/decorator.ts
+++ decorator/decorator.ts
@@ -1,17 +1,11 @@
 // Aggregation is a key principle behind Decorator
 
-/**
- * The base Component interface defines operations that can be altered by
- * decorators.
- */
+// Interface - pre-implemented drawMonster() function
 interface Component {
     drawMonster(): string;
 }
 
-/**
- * Concrete Components provide default implementations of the operations. There
- * might be several variations of these classes.
- */
+// Build a default monster
 class BasicMonster implements Component {
     public drawMonster(): string {
         return 'My basic monster has a head, body, two arms, and two legs.';
@@ -32,32 +26,18 @@ class Decorator implements Component {
         this.component = component;
     }
 
-    /**
-     * The Decorator delegates all work to the wrapped component.
-     */
     public drawMonster(): string {
         return this.component.drawMonster();
     }
 }
 
-/**
- * Concrete Decorators call the wrapped object and alter its result in some way.
- */
+// Create configurable monster attributes
 class TallMonster extends Decorator {
-    /**
-     * Decorators may call parent implementation of the operation, instead of
-     * calling the wrapped object directly. This approach simplifies extension
-     * of decorator classes.
-     */
     public drawMonster(): string {
         return `${super.drawMonster()}.. and now I'll add 100 feet of height to my monster`;
     }
 }
 
-/**
- * Decorators can execute their behavior either before or after the call to a
- * wrapped object.
- */
 class FuzzyMonster extends Decorator {
     public drawMonster(): string {
         return `${super.drawMonster()}.. and now I'll add fluffy fur to my monster`;
@@ -70,46 +50,31 @@ class ScaleyMonster extends Decorator {
     }
 }
 
-/**
- * The client code works with all objects using the Component interface. This
- * way it can stay independent of the concrete classes of components it works
- * with.
- */
-function clientCode(component: Component) {
-    // ...
-
+// Function for easy running of the code
+function runTheCodez(component: Component) {
     console.log(`${component.drawMonster()}`);
-
-    // ...
 }
 
-/**
- * This way the client code can support both simple components...
- */
+
+// Build various monster configurations
 const simple = new BasicMonster();
 console.log('I\'ve got a basic monster:');
-clientCode(simple);
+runTheCodez(simple);
 console.log('');
 
-/**
- * ...as well as decorated ones.
- *
- * Note how decorators can wrap not only simple components but the other
- * decorators as well.
- */
 const tall = new TallMonster(simple);
 const fuzzy = new FuzzyMonster(simple);
 const scaley = new ScaleyMonster(simple);
 const tallAndFuzzy = new FuzzyMonster(tall);
 const tallAndScaley = new TallMonster(scaley);
 console.log('Now I\'ve got a fuzzy monster:');
-clientCode(fuzzy);
+runTheCodez(fuzzy);
 console.log('')
 console.log('And here is a tall monster:');
-clientCode(tall);
+runTheCodez(tall);
 console.log('');
 console.log('Lookout! This monster is tall AND fuzzy!:');
-clientCode(tallAndFuzzy);
+runTheCodez(tallAndFuzzy);
 console.log('');
 console.log('Woah! This monster is scaley and tall!:');
-clientCode(tallAndScaley);
+runTheCodez(tallAndScaley);
